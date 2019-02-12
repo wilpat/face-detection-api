2f5616fbd5902ffc1a384fbc58b5e7674b42fb9c
William Okafor <wilpat456@gmail.com>
2019-02-12 21:19:29 +0100

    bug fix

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cors());
 const PORT = process.env.PORT ? process.env.PORT : 3001;
 
 app.get('/', (req, res) =>{
-	res.json('')
+	res.json('Welcome')
 });
 
 app.post('/signin', (req, res) => { user.login(req, res, db, bcrypt) });
